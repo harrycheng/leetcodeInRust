263c9a1f91c1a968975f75fdfe5b82a1b9b65896
HarryCheng <hc.harrycheng@gmail.com>
2024-01-09 17:53:19 +0800

    feat: first add

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+pub mod reverse_k_group;
+
+use crate::reverse_k_group::Solution;
+use crate::reverse_k_group::ListNode;
+
+fn main() {
+    println!("Hello, world!");
+    let xs: [i32; 5] = [1, 2, 3, 4, 5];
+    let  head=ListNode{
+        val = 1,
+        next= nil;
+
+
+    };
+
+    for i in &xs {
+        println!("{}", i);
+    }
+    
+}
